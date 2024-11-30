5350123f5fbafa1429455013b679169f555fa1c6
jsaezg <jsagtj@gmail.com>
2024-11-29 23:00:15 -0300

    cambios para evitar deteccion de bots

--- test_url.py
+++ test_url.py
@@ -8,6 +8,8 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument('--disable-gpu')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36")
+options.headless = False
 #options.add_argument('--remote-debugging-port=9222')
 
 def get_web(url='https://www.emol.com'):
